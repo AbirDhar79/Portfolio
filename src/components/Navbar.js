6b6eb16fddb07105c48326d840730412aacd0efe
Abir Dhar <abirdhar79@gmail.com>
2025-06-05 23:43:09 +0530

    Update Navbar links to point to the correct blog and GitHub portfolio URLs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             {/* <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="link to the blog website or any other website"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/AbirDhar79/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
